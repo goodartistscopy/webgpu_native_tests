8a156b8ec8fd91d982766166b5962133dfb706c8
Christophe Dehais <christophe.dehais@gmail.com>
2024-06-26 18:10:19 +0200

    Refactor texture read-back code

--- src/webgpu_helpers.cpp
+++ src/webgpu_helpers.cpp
@@ -2,123 +2,140 @@
 
 #include <cassert>
 
-const char* to_string(wgpu::TextureFormat format) {
-    const char* names[] = {   
-        "Undefined",
-        "R8Unorm",
-        "R8Snorm",
-        "R8Uint",
-        "R8Sint",
-        "R16Uint",
-        "R16Sint",
-        "R16Float",
-        "RG8Unorm",
-        "RG8Snorm",
-        "RG8Uint",
-        "RG8Sint",
-        "R32Float",
-        "R32Uint",
-        "R32Sint",
-        "RG16Uint",
-        "RG16Sint",
-        "RG16Float",
-        "RGBA8Unorm",
-        "RGBA8UnormSrgb",
-        "RGBA8Snorm",
-        "RGBA8Uint",
-        "RGBA8Sint",
-        "BGRA8Unorm",
-        "BGRA8UnormSrgb",
-        "RGB10A2Uint",
-        "RGB10A2Unorm",
-        "RG11B10Ufloat",
-        "RGB9E5Ufloat",
-        "RG32Float",
-        "RG32Uint",
-        "RG32Sint",
-        "RGBA16Uint",
-        "RGBA16Sint",
-        "RGBA16Float",
-        "RGBA32Float",
-        "RGBA32Uint",
-        "RGBA32Sint",
-        "Stencil8",
-        "Depth16Unorm",
-        "Depth24Plus",
-        "Depth24PlusStencil8",
-        "Depth32Float",
-        "Depth32FloatStencil8",
-        "BC1RGBAUnorm",
-        "BC1RGBAUnormSrgb",
-        "BC2RGBAUnorm",
-        "BC2RGBAUnormSrgb",
-        "BC3RGBAUnorm",
-        "BC3RGBAUnormSrgb",
-        "BC4RUnorm",
-        "BC4RSnorm",
-        "BC5RGUnorm",
-        "BC5RGSnorm",
-        "BC6HRGBUfloat",
-        "BC6HRGBFloat",
-        "BC7RGBAUnorm",
-        "BC7RGBAUnormSrgb",
-        "ETC2RGB8Unorm",
-        "ETC2RGB8UnormSrgb",
-        "ETC2RGB8A1Unorm",
-        "ETC2RGB8A1UnormSrgb",
-        "ETC2RGBA8Unorm",
-        "ETC2RGBA8UnormSrgb",
-        "EACR11Unorm",
-        "EACR11Snorm",
-        "EACRG11Unorm",
-        "EACRG11Snorm",
-        "ASTC4x4Unorm",
-        "ASTC4x4UnormSrgb",
-        "ASTC5x4Unorm",
-        "ASTC5x4UnormSrgb",
-        "ASTC5x5Unorm",
-        "ASTC5x5UnormSrgb",
-        "ASTC6x5Unorm",
-        "ASTC6x5UnormSrgb",
-        "ASTC6x6Unorm",
-        "ASTC6x6UnormSrgb",
-        "ASTC8x5Unorm",
-        "ASTC8x5UnormSrgb",
-        "ASTC8x6Unorm",
-        "ASTC8x6UnormSrgb",
-        "ASTC8x8Unorm",
-        "ASTC8x8UnormSrgb",
-        "ASTC10x5Unorm",
-        "ASTC10x5UnormSrgb",
-        "ASTC10x6Unorm",
-        "ASTC10x6UnormSrgb",
-        "ASTC10x8Unorm",
-        "ASTC10x8UnormSrgb",
-        "ASTC10x10Unorm",
-        "ASTC10x10UnormSrgb",
-        "ASTC12x10Unorm",
-        "ASTC12x10UnormSrgb",
-        "ASTC12x12Unorm",
-        "ASTC12x12UnormSrgb",
-        "R16Unorm",
-        "RG16Unorm",
-        "RGBA16Unorm",
-        "R16Snorm",
-        "RG16Snorm",
-        "RGBA16Snorm",
-        "R8BG8Biplanar420Unorm",
-        "R10X6BG10X6Biplanar420Unorm",
-        "R8BG8A8Triplanar420Unorm",
-        "R8BG8Biplanar422Unorm",
-        "R8BG8Biplanar444Unorm",
-        "R10X6BG10X6Biplanar422Unorm",
-        "R10X6BG10X6Biplanar444Unorm",
-        "External",
-    };
-
-    assert(static_cast<uint32_t>(format) < sizeof(names));
-
-    return names[static_cast<uint32_t>(format)];
+uint32_t texture_format_size(wgpu::TextureFormat format) {
+    switch (format) {
+        case wgpu::TextureFormat::R8Unorm:
+        case wgpu::TextureFormat::R8Snorm:
+        case wgpu::TextureFormat::R8Uint:
+        case wgpu::TextureFormat::R8Sint:
+            return 1;
+        case wgpu::TextureFormat::R16Uint:
+        case wgpu::TextureFormat::R16Sint:
+        case wgpu::TextureFormat::R16Float:
+        case wgpu::TextureFormat::RG8Unorm:
+        case wgpu::TextureFormat::RG8Snorm:
+        case wgpu::TextureFormat::RG8Uint:
+        case wgpu::TextureFormat::RG8Sint:
+            return 2;
+        case wgpu::TextureFormat::R32Float:
+        case wgpu::TextureFormat::R32Uint:
+        case wgpu::TextureFormat::R32Sint:
+        case wgpu::TextureFormat::RG16Uint:
+        case wgpu::TextureFormat::RG16Sint:
+        case wgpu::TextureFormat::RG16Float:
+        case wgpu::TextureFormat::RGBA8Unorm:
+        case wgpu::TextureFormat::RGBA8UnormSrgb:
+        case wgpu::TextureFormat::RGBA8Snorm:
+        case wgpu::TextureFormat::RGBA8Uint:
+        case wgpu::TextureFormat::RGBA8Sint:
+        case wgpu::TextureFormat::BGRA8Unorm:
+        case wgpu::TextureFormat::BGRA8UnormSrgb:
+        case wgpu::TextureFormat::RGB10A2Uint:
+        case wgpu::TextureFormat::RGB10A2Unorm:
+        case wgpu::TextureFormat::RG11B10Ufloat:
+        case wgpu::TextureFormat::RGB9E5Ufloat:
+            return 4;
+        case wgpu::TextureFormat::RG32Float:
+        case wgpu::TextureFormat::RG32Uint:
+        case wgpu::TextureFormat::RG32Sint:
+        case wgpu::TextureFormat::RGBA16Uint:
+        case wgpu::TextureFormat::RGBA16Sint:
+        case wgpu::TextureFormat::RGBA16Float:
+            return 8;
+        case wgpu::TextureFormat::RGBA32Float:
+        case wgpu::TextureFormat::RGBA32Uint:
+        case wgpu::TextureFormat::RGBA32Sint:
+            return 16;
+        case wgpu::TextureFormat::Stencil8:
+            return 8;
+        case wgpu::TextureFormat::Depth16Unorm:
+            return 2;
+        case wgpu::TextureFormat::Depth24Plus:
+            assert(false && "unimplemented");
+            return 0;
+        case wgpu::TextureFormat::Depth24PlusStencil8:
+            assert(false && "unimplemented");
+            return 0;
+        case wgpu::TextureFormat::Depth32Float:
+            return 4;
+        case wgpu::TextureFormat::Depth32FloatStencil8:
+            assert(false && "unimplemented");
+            return 0;
+        case wgpu::TextureFormat::BC1RGBAUnorm:
+        case wgpu::TextureFormat::BC1RGBAUnormSrgb:
+        case wgpu::TextureFormat::BC2RGBAUnorm:
+        case wgpu::TextureFormat::BC2RGBAUnormSrgb:
+        case wgpu::TextureFormat::BC3RGBAUnorm:
+        case wgpu::TextureFormat::BC3RGBAUnormSrgb:
+        case wgpu::TextureFormat::BC4RUnorm:
+        case wgpu::TextureFormat::BC4RSnorm:
+        case wgpu::TextureFormat::BC5RGUnorm:
+        case wgpu::TextureFormat::BC5RGSnorm:
+        case wgpu::TextureFormat::BC6HRGBUfloat:
+        case wgpu::TextureFormat::BC6HRGBFloat:
+        case wgpu::TextureFormat::BC7RGBAUnorm:
+        case wgpu::TextureFormat::BC7RGBAUnormSrgb:
+        case wgpu::TextureFormat::ETC2RGB8Unorm:
+        case wgpu::TextureFormat::ETC2RGB8UnormSrgb:
+        case wgpu::TextureFormat::ETC2RGB8A1Unorm:
+        case wgpu::TextureFormat::ETC2RGB8A1UnormSrgb:
+        case wgpu::TextureFormat::ETC2RGBA8Unorm:
+        case wgpu::TextureFormat::ETC2RGBA8UnormSrgb:
+        case wgpu::TextureFormat::EACR11Unorm:
+        case wgpu::TextureFormat::EACR11Snorm:
+        case wgpu::TextureFormat::EACRG11Unorm:
+        case wgpu::TextureFormat::EACRG11Snorm:
+        case wgpu::TextureFormat::ASTC4x4Unorm:
+        case wgpu::TextureFormat::ASTC4x4UnormSrgb:
+        case wgpu::TextureFormat::ASTC5x4Unorm:
+        case wgpu::TextureFormat::ASTC5x4UnormSrgb:
+        case wgpu::TextureFormat::ASTC5x5Unorm:
+        case wgpu::TextureFormat::ASTC5x5UnormSrgb:
+        case wgpu::TextureFormat::ASTC6x5Unorm:
+        case wgpu::TextureFormat::ASTC6x5UnormSrgb:
+        case wgpu::TextureFormat::ASTC6x6Unorm:
+        case wgpu::TextureFormat::ASTC6x6UnormSrgb:
+        case wgpu::TextureFormat::ASTC8x5Unorm:
+        case wgpu::TextureFormat::ASTC8x5UnormSrgb:
+        case wgpu::TextureFormat::ASTC8x6Unorm:
+        case wgpu::TextureFormat::ASTC8x6UnormSrgb:
+        case wgpu::TextureFormat::ASTC8x8Unorm:
+        case wgpu::TextureFormat::ASTC8x8UnormSrgb:
+        case wgpu::TextureFormat::ASTC10x5Unorm:
+        case wgpu::TextureFormat::ASTC10x5UnormSrgb:
+        case wgpu::TextureFormat::ASTC10x6Unorm:
+        case wgpu::TextureFormat::ASTC10x6UnormSrgb:
+        case wgpu::TextureFormat::ASTC10x8Unorm:
+        case wgpu::TextureFormat::ASTC10x8UnormSrgb:
+        case wgpu::TextureFormat::ASTC10x10Unorm:
+        case wgpu::TextureFormat::ASTC10x10UnormSrgb:
+        case wgpu::TextureFormat::ASTC12x10Unorm:
+        case wgpu::TextureFormat::ASTC12x10UnormSrgb:
+        case wgpu::TextureFormat::ASTC12x12Unorm:
+        case wgpu::TextureFormat::ASTC12x12UnormSrgb:
+            assert(false && "unimplemented");
+            return 0;
+        case wgpu::TextureFormat::R16Unorm:
+            return 2;
+        case wgpu::TextureFormat::RG16Unorm:
+        case wgpu::TextureFormat::RGBA16Unorm:
+        case wgpu::TextureFormat::R16Snorm:
+        case wgpu::TextureFormat::RG16Snorm:
+            return 4;
+        case wgpu::TextureFormat::RGBA16Snorm:
+            return 8;
+        case wgpu::TextureFormat::R8BG8Biplanar420Unorm:
+        case wgpu::TextureFormat::R10X6BG10X6Biplanar420Unorm:
+        case wgpu::TextureFormat::R8BG8A8Triplanar420Unorm:
+        case wgpu::TextureFormat::R8BG8Biplanar422Unorm:
+        case wgpu::TextureFormat::R8BG8Biplanar444Unorm:
+        case wgpu::TextureFormat::R10X6BG10X6Biplanar422Unorm:
+        case wgpu::TextureFormat::R10X6BG10X6Biplanar444Unorm:
+            assert(false && "unimplemented");
+            return 0;
+        default:
+            return 0;
+    }
 }
 
 bool is_srgb(wgpu::TextureFormat format) {
@@ -150,4 +167,97 @@ bool is_srgb(wgpu::TextureFormat format) {
         default:
             return false;
     }
+}
+
+uint64_t get_buffer_size_for_texture_copy(wgpu::Texture texture, uint32_t& row_stride) {
+    row_stride = align_up<uint32_t>(texture.GetWidth() * texture_format_size(texture.GetFormat()), 256);
+    return row_stride * texture.GetHeight();
+}
+
+wgpu::ImageCopyBuffer image_copy_buffer_for_texture(const wgpu::Texture & texture, uint64_t & size) {
+    uint32_t row_stride = align_up<uint32_t>(texture.GetWidth() * texture_format_size(texture.GetFormat()), 256);
+    size = row_stride * texture.GetHeight();
+    wgpu::ImageCopyBuffer icb{
+        .layout = {
+            .offset = 0,
+            .bytesPerRow = row_stride,
+            .rowsPerImage = texture.GetHeight(),
+        },
+        .buffer = nullptr,
+    };
+    return icb;
+}
+
+void TextureCapture::pop() {
+    Capture& capture = this->captures[this->tail];
+
+    if (capture.buffer && capture.buffer.GetMapState() == wgpu::BufferMapState::Unmapped) {
+        wgpu::FutureWaitInfo read_back_info;
+        read_back_info.future = capture.buffer.MapAsync(wgpu::MapMode::Read, 0, capture.buffer.GetSize(), wgpu::CallbackMode::WaitAnyOnly,
+            [&capture](wgpu::MapAsyncStatus status, char const * message) {
+                if (status != wgpu::MapAsyncStatus::Success) {
+                    std::cout << "Buffer mapping error: " << status << "\n";
+                    return;
+                }
+
+                char const * data = reinterpret_cast<char const *>(capture.buffer.GetConstMappedRange());
+                capture.callback(data, capture.layout);
+
+                capture.buffer.Unmap();
+            }
+        );
+        auto status = this->device.GetAdapter().GetInstance().WaitAny(1, &read_back_info, 0);
+        if (status != wgpu::WaitStatus::Success || !read_back_info.completed) {
+            std::cout << "WaitAny failure: " << status << ". Buffer status = " << capture.buffer.GetMapState() << "\n";
+            return; // don't advance just yet
+        }
+    }
+
+    this->tail = (this->tail + 1) % this->count;
+}
+
+void TextureCapture::encode_texture_copy(wgpu::Texture const & texture, Capture & capture, wgpu::CommandEncoder encoder) {
+    uint64_t required_size;
+    wgpu::ImageCopyBuffer destination = image_copy_buffer_for_texture(texture, required_size);
+
+    uint64_t buffer_size = (capture.buffer) ? capture.buffer.GetSize() : 0;
+    if (buffer_size < required_size) {
+        wgpu::BufferDescriptor desc{
+            .label = "texture capture",
+            .usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::MapRead,
+            .size = required_size,
+        };
+        capture.buffer = device.CreateBuffer(&desc);
+    }
+    assert(capture.buffer);
+    destination.buffer = capture.buffer;
+
+    wgpu::ImageCopyTexture source{
+        .texture = texture,
+    };
+
+    wgpu::Extent3D extent{
+        .width = texture.GetWidth(),
+        .height = texture.GetHeight(),
+    };
+
+    bool submit = false;
+    if (!encoder) {
+        encoder = this->device.CreateCommandEncoder();
+        submit = true;
+    }
+
+    encoder.CopyTextureToBuffer(&source, &destination, &extent);
+
+    if (submit) {
+        wgpu::CommandBuffer cmds = encoder.Finish();
+        this->device.GetQueue().Submit(1, &cmds);
+    }
+
+    capture.layout = ImageDataLayout{
+      .format = texture.GetFormat(),
+      .width = texture.GetWidth(),
+      .height = texture.GetHeight(),
+      .row_stride = destination.layout.bytesPerRow,
+    };
 }
--- src/wgpu_test.cpp
+++ src/wgpu_test.cpp
@@ -1,12 +1,16 @@
+#include <_types/_uint32_t.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 #include <cassert>
 #include <chrono>
+#include <string>
 #include <unistd.h>
 #include <vector>
 
 #include <GLFW/glfw3.h>
-//#include <webgpu/webgpu_cpp.h>
 #include <webgpu/webgpu_glfw.h>
 
 #include "imgui_helpers.h"
@@ -57,7 +61,7 @@ struct GPU {
                 },
             },
             .uncapturedErrorCallbackInfo = {
-                .callback = [](WGPUErrorType type, char const * message, void * userdata) {
+                .callback = [](WGPUErrorType type, char const * message, void *) {
                     std::cout << "Error: " << message << "\n";
                 },
             },
@@ -76,8 +80,7 @@ struct GPU {
     return gpu;
 }
 
-void on_window_resized(GLFWwindow* window, int width, int height)
-{
+void on_window_resized(GLFWwindow* window, int width, int height) {
     GPU* gpu = reinterpret_cast<GPU*>(glfwGetWindowUserPointer(window));
     if (auto surface = gpu->surfaces.find(window); surface != gpu->surfaces.end()) {
         SurfaceConfiguration config{
@@ -89,9 +92,8 @@ void on_window_resized(GLFWwindow* window, int width, int height)
             .presentMode = PresentMode::Fifo,
         };
         surface->second.Configure(&config);
-        std::cout << "New size: " << width << "x" << height << "\n";
     } else {
-        std::cout << "resize: surface not found\n";
+        std::cout << "Resize: surface not found\n";
     }
 }
 
@@ -120,7 +122,7 @@ GLFWwindow* create_window(GPU &gpu, int width, int height) {
         .presentMode = PresentMode::Fifo,
     };
     surface.Configure(&config);
-    
+
     gpu.surfaces[window] = surface;
 
     SurfaceCapabilities capabilities;
@@ -129,7 +131,7 @@ GLFWwindow* create_window(GPU &gpu, int width, int height) {
         std::cout << "Supported surface formats: [";
         for (int i = 0; i < capabilities.formatCount; i++) {
             auto format = capabilities.formats[i];
-            std::cout << to_string(format) << ((format == preferred_format) ? "*" : "")
+            std::cout << format << ((format == preferred_format) ? "*" : "")
                 << ((i + 1 < capabilities.formatCount) ? ", " : "");
         }
         std::cout << "]\n";
@@ -173,9 +175,9 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    
+
     auto gpu = init_webgpu();
-    
+
     ImGuiWebGPU imgui(gpu.device);
 
     GLFWwindow* window = create_window(gpu, 1024, 768);
@@ -187,13 +189,13 @@ int main(int argc, char *argv[]) {
 
     ShaderModuleWGSLDescriptor wgsl_desc;
     wgsl_desc.code = shader_code;
-   
+
     ShaderModuleDescriptor shader_desc{
         .nextInChain = &wgsl_desc,
     };
-    
+
     ShaderModule shader_module = gpu.device.CreateShaderModule(&shader_desc);
-    
+
     ColorTargetState color_target_state{
         .format = TextureFormat::BGRA8Unorm
     };
@@ -210,6 +212,12 @@ int main(int argc, char *argv[]) {
     };
     RenderPipeline pipeline = gpu.device.CreateRenderPipeline(&descriptor);
 
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    TextureCapture capture(gpu.device);
+
+    int frame_id = 0;
     while (!glfwWindowShouldClose(window)) {
         std::vector<CommandBuffer> commands;
 
@@ -239,11 +247,13 @@ int main(int argc, char *argv[]) {
         pass.SetPipeline(pipeline);
         pass.Draw(3);
         pass.End();
-        commands.push_back(encoder.Finish());
 
-#if 0
+        commands.push_back(encoder.Finish());
+        gpu.device.GetQueue().Submit(commands.size(), commands.data());
+
+#if 1
         imgui.begin_frame(surface_info.texture.GetWidth(), surface_info.texture.GetHeight());
-        
+
         if (ImGui::BeginMenu("File", true))
         {
             if (ImGui::MenuItem("Open asset...", nullptr, false))
@@ -261,80 +271,32 @@ int main(int argc, char *argv[]) {
         imgui.end_frame(surface_info.texture);
 #endif
 
-#if 1
-        CommandEncoder rb_encoder = gpu.device.CreateCommandEncoder();
+        capture.push(surface_info.texture, nullptr,
+            [frame_id](char const * image_data, ImageDataLayout const& layout) {
+               std::stringstream file_name;
+               file_name << "capture_" << std::setfill('0') << std::setw(4) << std::to_string(frame_id) << ".ppm";
 
-        // Read back
-        uint32_t texture_size = surface_info.texture.GetWidth() * surface_info.texture.GetHeight() * 4;
-        BufferDescriptor desc{
-            .label = "readback",
-            .usage = BufferUsage::CopyDst | BufferUsage::MapRead,
-            .size = texture_size, 
-        };
-        Buffer read_back_buffer = gpu.device.CreateBuffer(&desc);
+               std::ofstream file(file_name.str());
+               file << "P6\n" << layout.width << " " << layout.height << "\n" << 255 << "\n";
+               for (int row = 0; row <layout.height; row++) {
+                    char const * pixel = image_data + row * layout.row_stride;
+                    for (int col = 0; col <layout.width; col++, pixel += 4) {
+                        if (layout.format == TextureFormat::BGRA8Unorm) {
+                            char rgb[3] = { *(pixel + 2), *(pixel + 1), *pixel };
+                            file.write(rgb, 3);
+                        } else {
+                            file.write(pixel, 3);
+                        }
+                   }
+               }
+        });
 
-        ImageCopyTexture source{
-            .texture = surface_info.texture,
-        };
-        ImageCopyBuffer destination{
-            .layout = {
-                .offset = 0,
-                .bytesPerRow = surface_info.texture.GetWidth() * 4,
-                .rowsPerImage = surface_info.texture.GetHeight(),
-            },
-            .buffer = read_back_buffer,
-        };
-        Extent3D extent{
-            .width = surface_info.texture.GetWidth(),
-            .height = surface_info.texture.GetHeight(),
-        };
-        rb_encoder.CopyTextureToBuffer(&source, &destination, &extent);
-
-        commands.push_back(rb_encoder.Finish());
-        gpu.device.GetQueue().Submit(commands.size(), commands.data());
-
-        BufferMapCallbackInfo map_info{
-            //.mode = CallbackMode::WaitAnyOnly,  // -> always times out, cb never called
-            .mode = CallbackMode::AllowSpontaneous, // -> future sometimes complete (even without calling ProcesseEvents())
-            //.mode = CallbackMode::AllowProcessEvents, // -> future sometimes complete (even without calling ProcesseEvents())
-            .callback = [](WGPUBufferMapAsyncStatus status, void *userdata) {
-                if (status != WGPUBufferMapAsyncStatus_Success) {
-                    std::cout << "Readback callback error: " << static_cast<int>(status) << "\n";
-                    return;
-                }
-                Buffer *read_back_buffer = reinterpret_cast<Buffer*>(userdata);
-
-                const char *data = reinterpret_cast<const char*>(read_back_buffer->GetConstMappedRange());
-                std::cout << "Saving capture\n";
-                std::ofstream file("capture.ppm");
-                file << "P6\n" << 1024 << " " << 768 << "\n" << 255 << "\n";
-                const char* pixel = data;
-                for (int i = 0; i < 1024 * 768; i++, pixel += 4) {
-                    char rgb[3] = { *(pixel + 2), *(pixel + 1), *pixel };
-                    file.write(rgb, 3);
-                }
-                delete read_back_buffer;
-            },
-            /* .userdata = &read_back_buffer, */
-            .userdata = new Buffer(read_back_buffer),
-        };
-        FutureWaitInfo read_back_info;
-        // note: non 0 timeout values are not supported in any modes
-        read_back_info.future = read_back_buffer.MapAsync(MapMode::Read, 0, texture_size, map_info);
-        auto status = gpu.instance.WaitAny(1, &read_back_info, 0);
-        if (status != WaitStatus::Success || !read_back_info.completed) {
-            std::cout << "Future did not complete, status = " << static_cast<int>(status) << "\n";
-        } /*else {
-            std::cout << "future completed\n";
-        }*/
-#else
-        gpu.device.GetQueue().Submit(commands.size(), commands.data());
-#endif
-        
         surface.Present();
-        
+
         // Poll for and process events
-        gpu.instance.ProcessEvents();   
+        gpu.instance.ProcessEvents();
+
+        capture.pop();
 
         // simple fps counter
         static int frame_count = 0;
@@ -348,11 +310,12 @@ int main(int argc, char *argv[]) {
         } else {
             frame_count++;
         }
+
+        frame_id++;
     }
-   
+
     gpu.release();
     glfwTerminate();
 
     return 0;
 }
-
